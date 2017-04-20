b58eac7603f303cd1933cd620f953616170ce78a
Yi Ou <Yi.Ou@utah.edu>
2017-04-20 03:45:18 -0600

    Fixes #15

--- js/coreset.js
+++ js/coreset.js
@@ -15,7 +15,7 @@ var sorted_data_list = {"Kentucky":"../data/ken_sort.txt",
 
 var STD_list = {"Kentucky": 0.07,
                 "Philadelphia Crimes": 0.003,
-                "Japan": 0.25};
+                "Japan": 0.45};
 
 var delta_list = {"Kentucky": 0.04,
                   "Philadelphia Crimes":0.002,
