293180f8a52748313c3affaf71beadcbbfd87027
Yi Ou <Yi.Ou@utah.edu>
2017-04-21 01:04:45 -0600

    Fixes #9

--- js/fullset.js
+++ js/fullset.js
@@ -20,8 +20,6 @@ var STD_list = {"Kentucky": 0.07,
                 "Philadelphia Crimes": 0.003,
                 "Japan": 0.43};
 
-
-
 var delta_list = {"Kentucky": 0.04,
                   "Philadelphia Crimes":0.002,
                   "Japan": 0.1};
